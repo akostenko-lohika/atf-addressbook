09213082bea849d7269ba47576c63bc7228b79d8
akostenko-lohika <akostenko@lohika.com>
2016-03-17 14:08:12 +0000

    updated path

--- src/core/ATFcore.py
+++ src/core/ATFcore.py
@@ -64,7 +64,26 @@ def main():
     #print 'parameters[5]:'+parameters[5]
 
 
-    pytest.main('--junitxml '+results_dir+'/results.xml -v -x '+temp_test_location)
+    pytest.main(' --html=report.html --resultlog=path --junitxml '+results_dir+'/results.xml -v -x '+temp_test_location)
+    # Usage if pytest:
+    """
+py.test --maxfail=2    # stop after two failures
+
+py.test --showlocals # show local variables in tracebacks
+py.test -l           # show local variables (shortcut)
+
+py.test --tb=auto    # (default) 'long' tracebacks for the first and last
+                     # entry, but 'short' style for the other entries
+py.test --tb=long    # exhaustive, informative traceback formatting
+py.test --tb=short   # shorter traceback format
+py.test --tb=line    # only one line per failure
+py.test --tb=native  # Python standard library formatting
+py.test --tb=no      # no traceback at all
+
+py.test --resultlog=path
+
+
+    """
 
 if __name__ == '__main__':
     main()
