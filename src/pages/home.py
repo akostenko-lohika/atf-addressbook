2e5415d9cc75b276e834d04216fb8ba35d3f9e09
akostenko-lohika <akostenko@lohika.com>
2016-03-18 16:12:27 +0200

    updating home page class

--- src/pages/home.py
+++ src/pages/home.py
@@ -2,11 +2,20 @@
 """
 from pages.base_page import BasePage
 from locators.locators import HomePageLocators
-from elements.elements import InputText
+from elements.elements import InputText, WebComponent
+from selenium.webdriver.common.by import By
 
 
 class HomePage(BasePage):
     """Home page"""
+
+    sort_column = (By.CSS_SELECTOR, "#search-az>form>input")
+
+
     def search_on_page(self, search_string):
         """Search some string in search filter."""
         InputText(self.driver, HomePageLocators.search_field).set_value(search_string)
+
+    def sort_table(self, sort_column):
+        WebComponent(self.driver,sort_column).click()
+        pass
